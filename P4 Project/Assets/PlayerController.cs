d6aab470edd5e0ab6e56b93e60e29650435b2d52
Weezle0 <wesselbrattinga@gmail.com>
2021-04-19 10:57:31 +0200

    Beter funcionerende sprint

--- P4 Project/Assets/PlayerController.cs
+++ P4 Project/Assets/PlayerController.cs
@@ -70,13 +70,9 @@ public class PlayerController : MonoBehaviour
             isSprinting = false;
         }
 
-        if (isSprinting == true && isWalking == true)
+        if (isSprinting == true && isWalking == true && isGrounded == true)
         {
             runningTimer += 1 * Time.deltaTime;
-        }
-        else
-        {
-            runningTimer = 0;
         }
 
         if (runningTimer >= maxRunningTimer)
